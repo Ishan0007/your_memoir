355e2c04745084c54b63107b64d3cf7c17a97426
Ishan0007 <ishan.ver30@gmail.com>
2022-08-28 02:13:30 -0700

    auth bug fix

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://memoir-app-v1.herokuapp.com/' });
+const API = axios.create({ baseURL: 'https://mymemoir.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
